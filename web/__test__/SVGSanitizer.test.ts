faffa21797f84377bed9fca33ad8782a2dac2e5f
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-02-26 22:40:45 +0100

    reorganized svg test folder

--- web/__test__/SVGSanitizer.test.ts
+++ web/__test__/SVGSanitizer.test.ts
@@ -7,8 +7,8 @@ import * as fs from 'fs';
 //TEST CON SVG FORNITO DA DOMPURIFY
 describe('Sanitize SVG API', () => {
   it('should sanitize SVG (basic example)', async () => {
-    const dirtySVG = fs.readFileSync(`${__dirname}/svg_test_files/NOTSanitizedSVG.svg`, 'utf-8');
-    const expectedCleanSVG = fs.readFileSync(`${__dirname}/svg_test_files/sanitizedSVG.svg`, 'utf-8').replace(/\s/g, '');    
+    const dirtySVG = fs.readFileSync(`${__dirname}/svg_test_files/testDompurify/dompurify_NOT_sanitized.svg`, 'utf-8');
+    const expectedCleanSVG = fs.readFileSync(`${__dirname}/svg_test_files/testDompurify/dompurify_sanitized.svg`, 'utf-8').replace(/\s/g, '');    
 
     const { req, res } = createMocks<NextApiRequest, NextApiResponse>({
       method: 'POST',
@@ -23,8 +23,8 @@ describe('Sanitize SVG API', () => {
 //TEST CON SVG DI SANMARCO INFORMATICA
 describe('Sanitize SVG API', () => {
     it('should sanitize SVG (Sanmarco Informatica\'s example)', async () => {
-      const dirtySVG = fs.readFileSync(`${__dirname}/svg_test_files/MateriaPrima.svg`, 'utf-8');
-      const expectedCleanSVG = fs.readFileSync(`${__dirname}/svg_test_files/sanitized_MateriaPrima.svg`, 'utf-8').replace(/\s/g, '');    
+      const dirtySVG = fs.readFileSync(`${__dirname}/svg_test_files/testSanmarcoInformatica/materiaPrima_NOT_sanitized.svg`, 'utf-8');
+      const expectedCleanSVG = fs.readFileSync(`${__dirname}/svg_test_files/testSanmarcoInformatica/materiaPrima_sanitized.svg`, 'utf-8').replace(/\s/g, '');    
   
       const { req, res } = createMocks<NextApiRequest, NextApiResponse>({
         method: 'POST',
