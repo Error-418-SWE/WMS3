5abc0c6b352bb2dee5678206c1a94608de3455c7
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-02-26 20:37:21 +0100

    creazione dell'API per la sanitizzazione

---
+++ web/pages/api/SVGSanitizer.ts
@@ -0,0 +1,15 @@
+// pages/api/sanitize.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+import createDOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom';
+
+export default async function sanitize(req: NextApiRequest, res: NextApiResponse) {
+  const { svg } = req.body;
+
+  const window = new JSDOM('').window;
+  const DOMPurify = createDOMPurify(window);
+
+  const cleanSVG = DOMPurify.sanitize(svg);
+
+  res.status(200).json({ cleanSVG });
+}
