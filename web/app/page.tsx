457a7ac3d35872c434294b70d4b694a10f2ad01b
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-02-27 16:47:22 +0100

    created first page (no logic)

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,47 @@
+import { Button } from "@/components/ui/button";
 import styles from "./page.module.css";
 
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1>Ciao, WMS3!</h1>
-    </main>
-  );
+	return (
+		<main className={"h-screen flex items-center justify-center"}>
+			<Card className={"max-w-sm mx-auto"}>
+				<CardHeader>
+					<CardTitle className={"text-4xl"}>
+						Definizione dell'ambiente 3D
+					</CardTitle>
+				</CardHeader>
+				<CardContent className={"flex flex-col gap-y-5"}>
+					<Card className={"rounded-md"}>
+						<CardHeader>
+							<CardTitle>Planimetria rettangolare</CardTitle>
+							<CardDescription>
+								Definisci le dimensioni del magazzino.
+								<br />È possibile definire solo magazzini a pianta rettangolare.
+							</CardDescription>
+						</CardHeader>
+					</Card>
+					<Card className={"rounded-md"}>
+						<CardHeader>
+							<CardTitle>Planimetria personalizzata</CardTitle>
+							<CardDescription>
+								Carica la planimetria o parti da un magazzino predefinito.
+							</CardDescription>
+						</CardHeader>
+					</Card>
+				</CardContent>
+				<CardFooter className={"justify-end"}>
+					<Button>Continua</Button>
+				</CardFooter>
+			</Card>
+		</main>
+	);
 }
