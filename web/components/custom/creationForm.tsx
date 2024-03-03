fdbe464270bb012296f312ea92577545008a8254
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-04 00:56:00 +0100

    fix import missing #patch

--- web/components/custom/creationForm.tsx
+++ web/components/custom/creationForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { use, useContext, useState } from "react";
+import { SetStateAction, useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Button, buttonVariants } from "@/components/ui/button";
