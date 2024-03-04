15f3ed586e43b7600702c4db4db0feadb70731e3
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-04 20:15:09 +0100

    implemented panel visualizzation logic

--- web/app/app/page.tsx
+++ web/app/app/page.tsx
@@ -1,26 +1,77 @@
+"use client";
+
+import OrdersPanel from "@/components/custom/panels/ordersPanel";
+import ProductsPanel from "@/components/custom/panels/productsPanel";
+import ZonePanel from "@/components/custom/panels/zonePanel";
 import { Button } from "@/components/ui/button";
-import Image from 'next/image';
+import Image from "next/image";
+import { useState } from "react";
+import { set } from "zod";
+
+const iconSize = 30;
 
 export default function app() {
+	const [showPanel, setShowPanel] = useState(false);
+	const [panel, setPanel] = useState(<></>);
+
 	return (
 		<main className={"h-screen flex"}>
-			<nav className="flex-col h-screen bg-primary pt-2 items-center">
+			<nav className="flex flex-col h-screen bg-primary pt-2 items-center gap-6">
 				<Image
 					src="/icons/logo.svg"
 					alt="logo"
-					width={40}
-					height={40}
+					width={50}
+					height={50}
+					className="flex flex-col items-center"
+					priority
 				/>
-				<Button className="flex-col">
+				<Button
+					onClick={() => {
+						setPanel(<ZonePanel />);
+						setShowPanel(panel.type !== ZonePanel || !showPanel);
+					}}
+					className="flex flex-col items-center"
+				>
 					<Image
 						src="/icons/zone.svg"
-						alt=""
-						width={24}
-						height={24}
+						alt="Zone"
+						width={iconSize}
+						height={iconSize}
 					/>
 					<span>Zone</span>
 				</Button>
+				<Button
+					onClick={() => {
+						setPanel(<ProductsPanel />);
+						setShowPanel(panel.type !== ProductsPanel || !showPanel);
+					}}
+					className="flex flex-col items-center"
+				>
+					<Image
+						src="/icons/products.svg"
+						alt="products"
+						width={iconSize}
+						height={iconSize}
+					/>
+					<span>Products</span>
+				</Button>
+				<Button
+					onClick={() => {
+						setPanel(<OrdersPanel />);
+						setShowPanel(panel.type !== OrdersPanel || !showPanel);
+					}}
+					className="flex flex-col items-center"
+				>
+					<Image
+						src="/icons/orders.svg"
+						alt="products"
+						width={iconSize}
+						height={iconSize}
+					/>
+					<span>Orders</span>
+				</Button>
 			</nav>
+			{showPanel && panel}
 		</main>
 	);
 }
---
+++ web/components/custom/panels/ordersPanel.tsx
@@ -0,0 +1,9 @@
+export default function ZonePanel() {
+    return (
+        <aside className="flex flex-col w-1/4 bg-gray-200">
+            <div>
+                Orders
+            </div>
+        </aside>
+    );
+}
