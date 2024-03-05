6593cebe76b5c0d34aca750885be486b897dbe2f
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-06 00:40:56 +0100

    change other panels folder

---
+++ web/components/custom/panels/Settings/settingsPanel.tsx
@@ -0,0 +1,15 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+export default function ZonePanel() {
+	return (
+		<aside className={"w-1/5 h-screen shadow-xl shrink-0 z-10  absolute bg-secondary"}>
+			<div className={"m-5"}>
+				<h1 className={"font-bold text-2xl"}>Impostazioni</h1>
+				<h2 className={"font-bold mt-4"}>Informazioni</h2>
+				<p>Versione WMS3</p>
+                <h2 className={"font-bold mt-4"}>Demo</h2>
+                <p>Reimposta la demo di WMS3. <br/> Non è possibile annullare l&apos;operazione</p>
+                <Button className={buttonVariants({ variant: "destructive" }) + " mt-2 float-right"}>Reimposta</Button>
+            </div>
+		</aside>
+	);
+}
