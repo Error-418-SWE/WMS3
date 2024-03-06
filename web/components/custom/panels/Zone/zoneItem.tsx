2ba9c973411b63fa8ca3d32780920d73767b2638
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2024-03-06 20:05:34 +0100

    added delete zone dialog

--- web/components/custom/panels/Zone/zoneItem.tsx
+++ web/components/custom/panels/Zone/zoneItem.tsx
@@ -2,6 +2,7 @@ import { Button, buttonVariants } from "@/components/ui/button";
 import { Zone } from "@/model/zone";
 import Image from "next/image";
 import ZoneItemDetails from "./zoneItemDetails";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 const imageButtonSize = 15;
 
@@ -15,24 +16,31 @@ export default function ZoneItem({ zone }: ZoneItemProps) {
 			className={"flex p-3 gap-3 items-center hover:bg-slate-300 rounded-md"}
 		>
 			<span className={"grow font-bold"}>{zone.getId()}</span>
-            <ZoneItemDetails zone={zone} />
-			<Button
-				onClick={() => {
-					//TODO logic
-					console.log(zone);
-				}}
-				className={
-					buttonVariants({ variant: "secondary" }) +
-					"bg-transparent shadow-none"
-				}
-			>
-				<Image
-					src="/icons/delete.svg"
-					width={imageButtonSize}
-					height={imageButtonSize}
-					alt="Add"
-				/>
-			</Button>
+			<ZoneItemDetails zone={zone} />
+			<Dialog>
+				<DialogTrigger >
+					<Image
+						src="/icons/delete.svg"
+						width={imageButtonSize}
+						height={imageButtonSize}
+						alt="Add"
+					/>
+				</DialogTrigger>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Eliminazione scaffale</DialogTitle>
+						<DialogDescription>
+							Questa azione non può essere annullata. Sei sicuro di voler eliminare la zona {zone.getId()}?
+						</DialogDescription>
+					</DialogHeader>
+					<Button onClick={() => {
+						//TODO elimination logic
+						console.log("Elimina zona con id: " + zone.getId());
+					}}className={buttonVariants({variant: "destructive"}) + " w-min ml-auto"}>Elimina</Button>
+				</DialogContent>
+			</Dialog>
+
+
 		</div>
 	);
 }
