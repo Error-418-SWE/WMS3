95c948b973a7b8355f4210893066575fe1017368
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-08 00:24:50 +0100

    removed commented part #patch

--- web/dataRepository/zoneRepository.ts
+++ web/dataRepository/zoneRepository.ts
@@ -1,33 +1,7 @@
 import {Zone} from "@/model/zone";
 import { ZoneMapper } from "@/dataMapper/zoneMapper";
-import { BinMapper } from "@/dataMapper/binMapper";
 
 export class ZoneRepository{
-    /*public static getAllZones(): Promise<Zone[]> {
-        return fetch("/api/zones")
-            .then(response => response.json())
-            .then(json => json.map((zone: any) => ZoneMapper.toDomain(zone)))
-            .then(zones => {
-                return Promise.all(zones.map((zone: { id: any; bins: any; }) => {
-                    return fetch(`/api/binofzone/${zone.id}`)
-                        .then(response => response.json())
-                        .then(bins => {
-                            return Promise.all(bins.map((bin: any) => {
-                                return fetch(`/api/products/${bin.product_id}`)
-                                    .then(response => response.json())
-                                    .then(product => {
-                                        return BinMapper.toDomain({...bin, product});
-                                    });
-                            }))
-                            .then(mappedBins => {
-                                zone.bins = mappedBins;
-                                return zone;
-                            });
-                        });
-                }));
-            });
-    }*/
-
     public static getAllZones(): Promise<Zone[]> {
         return fetch("/api/zones")
             .then(response => response.json())
