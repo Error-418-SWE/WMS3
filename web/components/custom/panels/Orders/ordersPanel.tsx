c5a2f75bb57bfe74996fda420f93529f8b69822a
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-08 01:32:35 +0100

    removed unused useSearchParams hook

--- web/components/custom/panels/Orders/ordersPanel.tsx
+++ web/components/custom/panels/Orders/ordersPanel.tsx
@@ -1,15 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { useSearchParams } from "next/navigation";
 
-export default function ZonePanel() {
-    
-    const searchParams = useSearchParams();
-
-    searchParams?.forEach((value, key) => {
-        console.log(key, value);
-    });
-    
+export default function ZonePanel() {    
     return (
         <aside className={"flex flex-col h-screen w-1/5 shadow-xl shrink-0 z-10  absolute bg-secondary"}>
             <div className={"flex m-5 items-start"}>
