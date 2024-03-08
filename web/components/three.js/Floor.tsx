0bc66014ebc98544e2b38bd02125adcac9a251b6
Antonio <oselieroantonio@gmail.com>
2024-03-08 14:45:20 +0100

    parametri resi variabili costanti

--- web/components/three.js/Floor.tsx
+++ web/components/three.js/Floor.tsx
@@ -1,6 +1,18 @@
 import { useSearchParams } from "next/navigation";
 import { DoubleSide } from "three";
 
+const position_x=0.1;
+const position_y=-0.5 * Math.PI;
+const position_z=0;
+
+const euler_x=Math.PI / 2;
+const euler_y=0;
+const euler_z=0;
+
+const scale_x=2;
+const scale_y=2;
+const scale_z=2;
+
 const choice_mode = useSearchParams()?.get("choice");
 const floor_width: number = +useSearchParams()?.get("larghezza")!;
 const floor_depth: number = +useSearchParams()?.get("profondita")!;
@@ -15,9 +27,9 @@ export default function Floor() {
 			) {
 			return (
 				<mesh
-					position={[0.1, -0.5 * Math.PI, 0]}
-					rotation={[Math.PI / 2, 0, 0]}
-					scale={[2, 2, 2]}
+					position={[position_x, position_y, position_z]}
+					rotation={[euler_x, euler_y, euler_z]}
+					scale={[scale_x, scale_y, scale_z]}
 				>
 					<planeGeometry args={[floor_width, floor_depth]} />
 					<meshBasicMaterial color="white" side={DoubleSide} />
