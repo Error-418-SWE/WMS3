1d2438013c108d4a42fc5d029624f6a8578d04b4
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-12 01:38:11 +0100

    aggiunto stile agli span e aggiunta visualizzazione attributo height del prodotto

--- web/components/custom/panels/Products/productItemDetails.tsx
+++ web/components/custom/panels/Products/productItemDetails.tsx
@@ -8,31 +8,42 @@ interface ProductItemProps {
 }
 
 export default function ProductItemDetails({ product }: ProductItemProps) {
-
 	const { setElementDetails, setShowElementDetails } = useElementDetails();
 
 	return (
 		<div className={"flex flex-col h-full mx-5"}>
 			<div className={"flex items-center mt-2 justify-between"}>
-			  <span className={"font-bold"}>{product.getName()}</span>
-			  <Button className={buttonVariants({variant : "secondary"}) + " border"} onClick={() => {
-				setShowElementDetails(false);
-			  }}>
-				X
-			  </Button>
+				<span className={"font-bold"}>{product.getName()}</span>
+				<Button
+					className={buttonVariants({ variant: "secondary" }) + " border"}
+					onClick={() => {
+						setShowElementDetails(false);
+					}}
+				>
+					X
+				</Button>
 			</div>
-			<span className={"text-sm text-muted-foreground"}>Informazioni del prodotto</span>
+			<span className={"text-sm text-muted-foreground"}>
+				Informazioni del prodotto
+			</span>
 			<div className={"grid items-center grid-cols-3 grid-rows-3 gap-y-2 mt-2"}>
-		 		<Label>ID</Label><span className={"col-span-2"}>{product.getId()}</span>
-		 		<Label>Categorie</Label><span className={"col-span-2"}>Categorie</span>
-		 		<Label>Dimensioni</Label>
-		 		<span>{product.getLength()}</span>
-		 		<span>{product.getWidth()}</span>
-		 		<Label>Peso</Label>
-		 		<span className={"col-span-2"}>{product.getWeight()}</span>
+				<Label>ID</Label>
+				<span className={"col-span-2 dataSpan"}>{product.getId()}</span>
+				<Label>Categorie</Label>
+				<span className={"col-span-2"}>
+					{product.getCategories().join(", ")}
+				</span>
+				<Label>Dimensioni</Label>
+				<div className={"grid items-center grid-cols-3 col-span-2 gap-2"}>
+					<span className="dataSpan">{product.getLength()}</span>
+					<span className="dataSpan">{product.getWidth()}</span>
+					<span className="dataSpan">{product.getHeight()}</span>
+				</div>
+				<Label>Peso</Label>
+				<span className={"col-span-2 dataSpan"}>{product.getWeight()}</span>
 			</div>
 
-			<hr className={"my-5"}/>
-	  	</div>
+			<hr className={"my-5"} />
+		</div>
 	);
 }
