b3590c9766db17fcf7a19ea41983699a3eb9b21a
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-12 01:48:07 +0100

    rimosso pulsante reload da ordini di movimentazione

--- web/components/custom/panels/Orders/ordersPanel.tsx
+++ web/components/custom/panels/Orders/ordersPanel.tsx
@@ -1,19 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
 export default function ZonePanel() {    
     return (
         <aside className={"flex flex-col h-screen w-1/5 shadow-xl shrink-0 z-10  absolute bg-secondary"}>
             <div className={"flex m-5 items-start"}>
                 <h1 className={"grow font-bold text-2xl"}>Ordini di movimentazione</h1>
-                <Button>
-                    <Image 
-                        src="/icons/reload.svg"
-                        width={13}
-                        height={13}
-                        alt="Add"
-                    />
-                </Button>
             </div>
             <div id={"ordersList"}>
             </div>
