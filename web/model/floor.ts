23490e8ad630d521677f3878ebc0984f2117fb9e
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-12 18:40:51 +0100

    introdotto elemento di modello Floor

---
+++ web/model/floor.ts
@@ -0,0 +1,24 @@
+class Floor{
+    length: number;
+    width: number;
+    SVG: string;
+    constructor(length: number, width: number, svg: string){
+        this.length = length;
+        this.width = width;
+        this.SVG = svg;
+    }
+
+    public getLength(): number {
+        return this.length;
+    }
+
+    public getWidth(): number {
+        return this.width;
+    }
+
+    public getSVG(): string {
+        return this.SVG;
+    }
+}
+
+export { Floor }
