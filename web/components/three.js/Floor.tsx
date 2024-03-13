1f8f4241968aff18862f1dde925e5e29f3faed89
Antonio <oselieroantonio@gmail.com>
2024-03-13 12:48:02 +0100

    caricato SVG sul piano

--- web/components/three.js/Floor.tsx
+++ web/components/three.js/Floor.tsx
@@ -1,17 +1,33 @@
+import * as THREE from 'three';
 import { DoubleSide } from "three";
 import { useFloorData } from "../providers/floorProvider";
+import React from "react";
 
 const inclination = Math.PI / 2;
 
+const SvgPlane: React.FC<{svgContent: string}> = ({ svgContent }) => {
+	const texture= new THREE.TextureLoader().load(`data:image/svg+xml;base64,${btoa(svgContent)}`);
+	return (
+		<mesh position={[0.1, -inclination, -4]} rotation={[inclination, 0, 0]}>
+			<planeGeometry args={[10,10]}/>
+			<meshBasicMaterial map={texture} side={DoubleSide}/>
+		</mesh>
+	);
+};
 export default function Floor() {
 	const { floor } = useFloorData();
-	
-	if (floor) {
+
+	if (floor && floor.getSVG() == "") {
 		return (
 			<mesh position={[0.1, -inclination, -4]} rotation={[inclination, 0, 0]}>
 				<planeGeometry args={[floor.getWidth(), floor.getLength()]} />
 				<meshBasicMaterial color="white" side={DoubleSide} />
 			</mesh>
 		);
+	}
+	if (floor && floor.getSVG() != "") {
+		return (
+			<SvgPlane svgContent={floor.getSVG()!}/>
+		)
 	}
 }
