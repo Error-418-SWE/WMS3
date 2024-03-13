4121812ce487a51386bbab06b44e98d612b987b1
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-14 00:51:32 +0100

    integration test per le server action delle zone

---
+++ web/__test__/Integration Test/Zones/getAllZones.test.ts
@@ -0,0 +1,85 @@
+import { getAllZones } from "@/ServerActions/Zones/getAllZones";
+
+jest.mock("pg", () => {
+	const productJson = {
+		id: 1,
+		name: "prodotto",
+		weight: 1,
+		length: 1,
+		width: 1,
+		height: 1,
+		categories: ["category"],
+	};
+	const binJson = {
+		bin_id: 1,
+		level_order: 1,
+		column_order: 1,
+		bin_height: 1,
+		bin_length: 1,
+		bin_width: 1,
+		product_id: 1,
+        product: productJson
+	};
+	const zoneJson = {
+		id: 1,
+		xcoordinate: 1,
+		ycoordinate: 1,
+		height: 1,
+		length: 1,
+		width: 1,
+		orientation: true,
+	};
+	const mClient = {
+		release: jest.fn(),
+		query: jest
+			.fn()
+			.mockResolvedValueOnce({ rows: [zoneJson] }) // zone
+			.mockResolvedValueOnce({ rows: [binJson] }) // bin
+			.mockResolvedValueOnce({ rows: [productJson] }) // product
+			.mockResolvedValueOnce({ rows: [{ id: 1, zone_id: 1, width: 1 }] }) // column
+			.mockResolvedValueOnce({ rows: [{ id: 1, zone_id: 1, height: 1 }] }), // levels
+	};
+	const mPool = {
+		connect: jest.fn().mockResolvedValue(mClient),
+		query: jest.fn(),
+		release: jest.fn(),
+	};
+	return { Pool: jest.fn(() => mPool) };
+});
+
+describe("getAllZones", () => {
+	it("should get all zones", async () => {
+		const actualValue = await getAllZones();
+		expect(actualValue).toEqual([
+			{
+				id: 1,
+				xcoordinate: 1,
+				ycoordinate: 1,
+				height: 1,
+				length: 1,
+				width: 1,
+				bins: [
+					{
+						bin_id: 1,
+						level_order: 1,
+						column_order: 1,
+						bin_height: 1,
+						bin_length: 1,
+						bin_width: 1,
+						product_id: 1,
+                        product: {
+                            id: 1,
+                            name: "prodotto",
+                            weight: 1,
+                            length: 1,
+                            width: 1,
+                            height: 1,
+                            categories: ["category"],
+                        },
+					},
+				],
+				orientation: true,
+			},
+		]);
+	});
+});
