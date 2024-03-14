15f903b1b83e740757d9d576181fb2890df8f072
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2024-03-14 18:29:02 +0100

    aggiunto evento onClick per il bin

--- web/components/three.js/Model3D/bin3D.tsx
+++ web/components/three.js/Model3D/bin3D.tsx
@@ -5,14 +5,17 @@ import * as THREE from 'three';
 
 interface Bin3DProps {
 	bin: Bin;
+	position: THREE.Vector3;
 }
 
-export function Bin3D({ bin }: Bin3DProps) {
-	const binGeometry = new THREE.BoxGeometry(bin.getWidth(), bin.getLength(), bin.getHeight());
+export function Bin3D({ bin, position}: Bin3DProps) {
+	const binGeometry = new THREE.BoxGeometry(bin.getWidth(), bin.getHeight(), bin.getLength());
 	return (
-		<group>
+		<group position={position} onClick={ () =>
+			alert("Bin: " + bin.getId())
+		}>
 			<Box geometry={binGeometry}>
-				<meshBasicMaterial attach="material" color={0x4169e1} />
+				<meshBasicMaterial attach="material" color={bin.getProduct() ? 0x4169e1 : 0x5f5f5f} transparent={true} opacity={0.5}/>
 			</Box>
 			<Edges geometry={binGeometry} color="black" />
 		</group>
