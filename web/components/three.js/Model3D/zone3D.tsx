352df386cde8ae817849e9c19f06e37c39b36b55
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2024-03-14 18:29:43 +0100

    implementato comonente Zone3D #minor

---
+++ web/components/three.js/Model3D/zone3D.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Box, Edges } from '@react-three/drei';
+import { Zone } from '@/model/zone';
+import * as THREE from 'three';
+import { Bin3D } from './bin3D';
+
+interface Zone3DProps {
+	zone: Zone;
+	position: THREE.Vector3;
+}
+
+export function Zone3D({ zone, position}: Zone3DProps) {
+	const zoneGeometry = new THREE.BoxGeometry(zone.getWidth(), zone.getHeight(), zone.getLength());
+	return (
+		<group position={[position.x, position.y + zone.getHeight() / 2, position.z]}>
+			{
+				zone.getLevels().map((level, levelIndex) => {
+					let levelVerticalPosition = 0;
+					let levelsHeights = zone.getColumns()[0];
+					for (let i = 0; i < levelIndex; i++) {
+						levelVerticalPosition += levelsHeights[i].getHeight();
+					}
+					//each level is a level of the shelf.
+					return level.map((bin) => {
+						let binHorizontalPosition = 0;
+						for (let i = 0; i < bin.getColumn(); i++) {
+							binHorizontalPosition += (level[i].getWidth());
+						}
+						const binPosition = new THREE.Vector3(
+							binHorizontalPosition + bin.getWidth()/2 - zone.getWidth()/2,
+							levelVerticalPosition + bin.getHeight()/2 - zone.getHeight()/2,
+							bin.getLength()/2 - zone.getLength()/2
+						);
+						return <Bin3D bin={bin} position={binPosition} />
+					});
+				})
+			}
+			<Edges geometry={zoneGeometry} color="black" />
+		</group>
+	);
+}
+
+
+
+
