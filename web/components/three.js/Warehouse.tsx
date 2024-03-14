b81de326a0ab34fc79793f402bf4dd2cc7f4a264
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2024-03-14 14:09:20 +0100

    rimosso blocco suspanse aggiunto bin demo

--- web/components/three.js/Warehouse.tsx
+++ web/components/three.js/Warehouse.tsx
@@ -1,20 +1,22 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import Floor from "./Floor";
-import { Suspense } from "react";
+import Floor from "./Model3D/Floor";
 import { useFloorData } from "../providers/floorProvider";
+import { Bin } from "@/model/bin";
+import { Bin3D } from "./Model3D/bin3D";
 
 export default function Warehouse() {
-	const {floor} = useFloorData();
+	const { floor } = useFloorData();
 	return (
 		<div className="h-screen">
 			<Canvas
 				className={"h-screen, bg-BurlyWood"}
 			>
 				<PerspectiveCamera />
-				<Suspense>
-					<Floor/>
-				</Suspense>
+				<Floor />
+
+				<Bin3D bin={new Bin("1", 1, 1, 1, 1, 1, null)} />
+
 				<ambientLight />
 				<OrbitControls />
 			</Canvas>
