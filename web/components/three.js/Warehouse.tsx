546f7331fd8a20c441ee17700aeef3256a1565e9
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2024-03-14 18:30:29 +0100

    implementato posizioanemento degli scaffali

--- web/components/three.js/Warehouse.tsx
+++ web/components/three.js/Warehouse.tsx
@@ -4,23 +4,39 @@ import Floor from "./Model3D/Floor";
 import { useFloorData } from "../providers/floorProvider";
 import { Bin } from "@/model/bin";
 import { Bin3D } from "./Model3D/bin3D";
-
+import { Vector3 } from "three";
+import { useZonesData } from "../providers/zonesProvider";
+import { useBinsData } from "../providers/binsProvider";
+import { Zone } from "@/model/zone";
+import { Zone3D } from "./Model3D/zone3D";
 export default function Warehouse() {
 	const { floor } = useFloorData();
+	const { zones } = useZonesData();
+	const { bins } = useBinsData();
+
+	// Calculate the center of the warehouse
+	const center = new Vector3(floor.getWidth() / 2, 0, floor.getLength() / 2);
+
+	console.log("Warehouse", zones);
+
 	return (
 		<div className="h-screen">
 			<Canvas
 				className={"h-screen, bg-BurlyWood"}
 			>
-				<PerspectiveCamera />
+				<PerspectiveCamera makeDefault position={[floor.getWidth() / 2, 60, floor.getLength() * 1.5]} />
 				<Floor />
 
-				<Bin3D bin={new Bin("1", 1, 1, 1, 1, 1, null)} />
+				{
+					zones.map((zone: Zone, index: number) => {
+						const zonePosition = new Vector3(zone.getXcoordinate(), 0, zone.getYcoordinate());
+						return <Zone3D zone={zone} position={zonePosition} />
+					})
+				}
 
 				<ambientLight />
-				<OrbitControls />
+				<OrbitControls target={center} />
 			</Canvas>
 		</div>
 	);
 }
-
