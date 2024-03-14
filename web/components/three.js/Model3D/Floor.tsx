493f978d1c8bef4d00ec021bb597c9a6997f7e4c
Antonio <oselieroantonio@gmail.com>
2024-03-14 16:08:27 +0100

    lavoro ripristinato

--- web/components/three.js/Model3D/Floor.tsx
+++ web/components/three.js/Model3D/Floor.tsx
@@ -1,17 +1,57 @@
 import { DoubleSide } from "three";
 import { useFloorData } from "../../providers/floorProvider";
+import * as THREE from "three";
+import React from "react";
 
 const inclination = Math.PI / 2;
+
+interface SvgPlaneProps {
+	svgContent: string;
+	width: number;
+	length: number;
+}
+
+const SvgPlane: React.FC<SvgPlaneProps> = ({ svgContent, width, length }) => {
+	const texture = new THREE.TextureLoader().load(
+		`data:image/svg+xml;base64,${btoa(svgContent)}`
+	);
+	return (
+		<mesh position={[0.1, -inclination, -4]} rotation={[inclination, 0, 0]}>
+			<planeGeometry args={[width, length]} />
+			<meshBasicMaterial
+				color="white"
+				map={texture}
+				side={DoubleSide}
+				transparent={true}
+			/>
+		</mesh>
+	);
+};
 
 export default function Floor() {
 	const { floor } = useFloorData();
 
-	if (floor) {
+	if (floor && floor.getSVG() == "") {
 		return (
 			<mesh position={[0.1, -inclination, -4]} rotation={[inclination, 0, 0]}>
 				<planeGeometry args={[floor.getWidth(), floor.getLength()]} />
 				<meshBasicMaterial color="white" side={DoubleSide} />
 			</mesh>
+		);
+	}
+	if (floor && floor.getSVG() != "") {
+		return (
+			<group>
+				<mesh position={[0.1, -inclination, -4]} rotation={[inclination, 0, 0]}>
+					<planeGeometry args={[floor.getWidth(), floor.getLength()]} />
+					<meshBasicMaterial color="white" side={DoubleSide} />
+				</mesh>
+				<SvgPlane
+					svgContent={floor.getSVG()!}
+					width={floor.getWidth()!}
+					length={floor.getLength()!}
+				/>
+			</group>
 		);
 	}
 }
--- web/components/three.js/Warehouse.tsx
+++ web/components/three.js/Warehouse.tsx
@@ -4,23 +4,20 @@ import Floor from "./Model3D/Floor";
 import { useFloorData } from "../providers/floorProvider";
 import { Bin } from "@/model/bin";
 import { Bin3D } from "./Model3D/bin3D";
+import Camera from "./Camera";
 
 export default function Warehouse() {
 	const { floor } = useFloorData();
 	return (
 		<div className="h-screen">
-			<Canvas
-				className={"h-screen, bg-BurlyWood"}
-			>
-				<PerspectiveCamera />
+			<Canvas className={"h-screen, bg-BurlyWood"}>
+				<Camera />
 				<Floor />
 
 				<Bin3D bin={new Bin("1", 1, 1, 1, 1, 1, null)} />
 
 				<ambientLight />
-				<OrbitControls />
 			</Canvas>
 		</div>
 	);
 }
-
