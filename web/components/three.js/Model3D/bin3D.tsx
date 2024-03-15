4be3b33f231f68244cb8879314cfa1463f990fb6
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-15 04:09:20 +0100

    integrazione tra click sul bin e pannello informazioni

--- web/components/three.js/Model3D/bin3D.tsx
+++ web/components/three.js/Model3D/bin3D.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Edges } from '@react-three/drei';
 import { Bin } from '@/model/bin';
 import * as THREE from 'three';
+import { useElementDetails } from '@/components/providers/UI-Providers/ElementDetailsProvider';
+import BinItemDetails from '@/components/custom/panels/Bin/binItemDetails';
 
 interface Bin3DProps {
 	bin: Bin;
 	position: THREE.Vector3;
 }
 
-export function Bin3D({ bin, position}: Bin3DProps) {
-	const binGeometry = new THREE.BoxGeometry(bin.getWidth(), bin.getHeight(), bin.getLength());
-	return (
-		<group position={position} onClick={ () =>
-			alert("Bin: " + bin.getId())
-		}>
-			<Box geometry={binGeometry}>
-				<meshBasicMaterial attach="material" color={bin.getProduct() ? 0x4169e1 : 0x5f5f5f} transparent={true} opacity={0.5}/>
-			</Box>
-			<Edges geometry={binGeometry} color="black" />
-		</group>
-	);
+const selectedColor = 0xff0000;
+const defaultColor = 0x5f5f5f;
+const notEmptyColor = 0x4169e1;
+
+export function Bin3D({ bin, position }: Bin3DProps) {
+  const { elementDetails, setElementDetails, showElementDetails, setShowElementDetails } = useElementDetails();
+  const [isSelected, setIsSelected] = useState(false);
+
+  const binGeometry = new THREE.BoxGeometry(bin.getWidth(), bin.getHeight(), bin.getLength());
+
+  const handleClick = () => {
+    setIsSelected(true);
+    setElementDetails(<BinItemDetails bin={bin} />);
+    setShowElementDetails(true);
+  };
+
+  useEffect(() => {
+    if (!showElementDetails) {
+      setIsSelected(false);
+    }
+  }, [showElementDetails]);
+
+  return (
+    <group position={position} onClick={handleClick}>
+      <Box geometry={binGeometry}>
+        <meshBasicMaterial attach="material" color={isSelected ? selectedColor : (bin.getProduct() ? notEmptyColor : defaultColor)} transparent={true} opacity={0.5} />
+      </Box>
+      <Edges geometry={binGeometry} color="black" />
+    </group>
+  );
 }
