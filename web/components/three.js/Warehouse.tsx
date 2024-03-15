f527e5e1a2f401a249367ea4bd1e019680702b1d
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-15 04:08:51 +0100

    migliorati controlli e camera

--- web/components/three.js/Warehouse.tsx
+++ web/components/three.js/Warehouse.tsx
@@ -1,9 +1,10 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import {
+	OrbitControls,
+	PerspectiveCamera,
+} from "@react-three/drei";
 import Floor from "./Model3D/Floor";
 import { useFloorData } from "../providers/floorProvider";
-import { Bin } from "@/model/bin";
-import { Bin3D } from "./Model3D/bin3D";
 import { Vector3 } from "three";
 import { useZonesData } from "../providers/zonesProvider";
 import { useBinsData } from "../providers/binsProvider";
@@ -14,28 +15,32 @@ export default function Warehouse() {
 	const { zones } = useZonesData();
 	const { bins } = useBinsData();
 
-	// Calculate the center of the warehouse
-	const center = new Vector3(floor.getWidth() / 2, 0, floor.getLength() / 2);
-
-	console.log("Warehouse", zones);
-
 	return (
 		<div className="h-screen">
-			<Canvas
-				className={"h-screen, bg-BurlyWood"}
-			>
-				<PerspectiveCamera makeDefault position={[floor.getWidth() / 2, 60, floor.getLength() * 1.5]} />
+			<Canvas className={"h-screen, bg-BurlyWood"}>
+				<PerspectiveCamera
+					makeDefault
+					position={[floor.getWidth() / 2, 60, floor.getLength()]}
+					rotation={[Math.PI / 2, Math.PI / 2, Math.PI / 2]}
+				/>
 				<Floor />
 
-				{
-					zones.map((zone: Zone, index: number) => {
-						const zonePosition = new Vector3(zone.getXcoordinate(), 0, zone.getYcoordinate());
-						return <Zone3D zone={zone} position={zonePosition} />
-					})
-				}
+				{zones.map((zone: Zone, index: number) => {
+					const zonePosition = new Vector3(
+						zone.getXcoordinate(),
+						0,
+						zone.getYcoordinate()
+					);
+					return <Zone3D zone={zone} position={zonePosition} />;
+				})}
 
 				<ambientLight />
-				<OrbitControls target={center} />
+				<OrbitControls
+					dampingFactor={0.05}
+					screenSpacePanning={false}
+					minPolarAngle={Math.PI / 5}
+					maxPolarAngle={Math.PI / 2.3}
+				/>
 			</Canvas>
 		</div>
 	);
