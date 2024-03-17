d60b64dde95bd93538a279c52c78bf32d315db13
Antonio <oselieroantonio@gmail.com>
2024-03-17 17:40:52 +0100

    ripristinata camera

--- web/components/three.js/Warehouse.tsx
+++ web/components/three.js/Warehouse.tsx
@@ -4,19 +4,19 @@ import Floor from "./Model3D/Floor";
 import { useFloorData } from "../providers/floorProvider";
 import { Bin } from "@/model/bin";
 import { Bin3D } from "./Model3D/bin3D";
-import Camera from "./Camera";
 
 export default function Warehouse() {
 	const { floor } = useFloorData();
 	return (
 		<div className="h-screen">
 			<Canvas className={"h-screen, bg-BurlyWood"}>
-				<Camera />
+				<PerspectiveCamera />
 				<Floor />
 
 				<Bin3D bin={new Bin("1", 1, 1, 1, 1, 1, null)} />
 
 				<ambientLight />
+				<OrbitControls />
 			</Canvas>
 		</div>
 	);
