46d7d07b66802c961bde317ca9233fbedd27cee2
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2024-03-18 13:00:06 +0100

    fix selezione bin

--- web/components/three.js/Model3D/bin3D.tsx
+++ web/components/three.js/Model3D/bin3D.tsx
@@ -5,6 +5,7 @@ import * as THREE from 'three';
 import { useElementDetails } from '@/components/providers/UI-Providers/ElementDetailsProvider';
 import BinItemDetails from '@/components/custom/panels/Bin/binItemDetails';
 import { ThreeEvent } from '@react-three/fiber';
+import { useWarehouseData } from '@/components/providers/Threejs/warehouseProvider';
 
 interface Bin3DProps {
 	bin: Bin;
@@ -16,28 +17,22 @@ const defaultColor = 0x5f5f5f;
 const notEmptyColor = 0x4169e1;
 
 export function Bin3D({ bin, position }: Bin3DProps) {
-  const { elementDetails, setElementDetails, showElementDetails, setShowElementDetails } = useElementDetails();
-  const [isSelected, setIsSelected] = useState(false);
+  const { setElementDetails, setShowElementDetails } = useElementDetails();
+  const { selectedBin, setSelectedBin } = useWarehouseData();
 
   const binGeometry = new THREE.BoxGeometry(bin.getWidth(), bin.getHeight(), bin.getLength());
 
   const handleClick = (event : ThreeEvent<MouseEvent>) => {
     event.stopPropagation();
-    setIsSelected(true);
+		setSelectedBin(bin);
     setElementDetails(<BinItemDetails bin={bin} />);
     setShowElementDetails(true);
   };
 
-  useEffect(() => {
-    if (!showElementDetails) {
-      setIsSelected(false);
-    }
-  }, [showElementDetails]);
-
   return (
     <group position={position} onClick={handleClick}>
       <Box geometry={binGeometry}>
-        <meshBasicMaterial attach="material" color={isSelected ? selectedColor : (bin.getProduct() ? notEmptyColor : defaultColor)} transparent={true} opacity={0.5} />
+        <meshBasicMaterial attach="material" color={bin === selectedBin ? selectedColor : (bin.getProduct() ? notEmptyColor : defaultColor)} transparent={true} opacity={0.5} />
       </Box>
       <Edges geometry={binGeometry} color="black" />
     </group>
