a54ab9e9bed8cfc633bb75e3a568690f93d14d3e
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-19 10:12:56 +0100

    aggiunta di una grid basic non modificabile al piano

--- web/components/ThreeComponents/Model3D/Floor.tsx
+++ web/components/ThreeComponents/Model3D/Floor.tsx
@@ -1,5 +1,9 @@
-import { DoubleSide, Vector3 } from "three";
+import { DoubleSide, Vector3, GridHelper } from "three";
 import { useFloorData } from "../../providers/floorProvider";
+import { extend } from "@react-three/fiber";
+
+// This makes GridHelper usable as a JSX element.
+extend({ GridHelper });
 
 const inclination = Math.PI / 2;
 
@@ -8,10 +12,24 @@ export default function Floor() {
 
 	if (floor) {
 		return (
-			<mesh position={new Vector3(floor.getWidth() / 2, 0, floor.getLength() / 2)} rotation={[inclination, 0, 0]}>
-				<planeGeometry args={[floor.getWidth(), floor.getLength()]} />
-				<meshBasicMaterial color="white" side={DoubleSide} />
-			</mesh>
+			<>
+				<mesh
+					name="floor"
+					position={new Vector3(floor.getWidth() / 2, 0, floor.getLength() / 2)}
+					rotation={[inclination, 0, 0]}
+				>
+					<planeGeometry args={[floor.getWidth(), floor.getLength()]} />
+					<meshBasicMaterial side={DoubleSide} />
+				</mesh>
+				<gridHelper
+					args={[
+						floor.getWidth(), // size of the grid
+						floor.getWidth() / 2, // number of divisions
+					]}
+					position={[floor.getWidth() / 2, 0.01, floor.getLength() / 2]} // slightly above the floor
+					scale={[1, 1, floor.getLength() / floor.getWidth()]} // scale the grid to make it rectangular
+				/>
+			</>
 		);
 	}
 }
