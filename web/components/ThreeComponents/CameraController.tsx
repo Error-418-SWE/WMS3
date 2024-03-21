42dc26d64e2ab8bbb0cbca3c5a826752cb5d73cd
Giovanni <2914344+ggardin@users.noreply.github.com>
2024-03-21 23:44:06 +0100

    Creato codice spazzatura

--- web/components/ThreeComponents/CameraController.tsx
+++ web/components/ThreeComponents/CameraController.tsx
@@ -1,8 +1,6 @@
 import { useKeyboardControls } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { Vector3 } from "three";
-import { useFloorData } from "../providers/floorProvider";
-import { useState } from "react";
 
 interface CameraControllerProps {
 	setIsNavigating: (boolean: boolean) => void;
@@ -16,27 +14,23 @@ export function CameraController({
 
 	const [,get] = useKeyboardControls();
 	const camera = useThree();
-	const { floor } = useFloorData();
-	const moveSpeed = 0.25;
 
-	const [cameraPosition, setCameraPosition] = useState(camera.camera.position.clone());
-
-	useFrame(() => {
+	useFrame((state) => {
 		const { forward, backward, left, right, quick } = get()
+
 		setIsNavigating(forward || backward || left || right || false);
 
-		const speedModifier = quick ? 3 : 1;
-		const movement = new Vector3(
-			(right ? moveSpeed*speedModifier : 0) + (left ? -moveSpeed*speedModifier : 0),
-			0,
-			(backward ? moveSpeed*speedModifier : 0) + (forward ? -moveSpeed*speedModifier : 0)
+		const moveSpeed = quick ? 0.75 : 0.25;
+
+		state.camera.position.set(
+			camera.camera.position.x + (right ? moveSpeed : 0) + (left ? -moveSpeed : 0),
+			camera.camera.position.y,
+			camera.camera.position.z + (backward ? moveSpeed : 0) + (forward ? -moveSpeed : 0)
 		);
 
-		const newCameraPosition = camera.camera.position.clone().add(movement);
-		setCameraPosition(newCameraPosition);
-
-		camera.camera.position.copy(newCameraPosition);
-	})
-	console.log('Current camera position:', cameraPosition);
-	return null;
+		setCameraPosition(state.camera.position);
+	});
+	return (
+		<></>
+	);
 }
