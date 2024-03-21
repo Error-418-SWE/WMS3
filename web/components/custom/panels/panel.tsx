7dc0abb7783cbeef57a26e55b60e7645f1dcad52
Giovanni <2914344+ggardin@users.noreply.github.com>
2024-03-21 15:15:40 +0100

    Impostato stile pannello

--- web/components/custom/panels/panel.tsx
+++ web/components/custom/panels/panel.tsx
@@ -8,10 +8,10 @@ export default function Panel({children, className} : PanelProps) {
 	return (
 		<aside
 			className = {
-				"flex flex-col h-screen w-1/5 h-screen shadow-xl max-h-screen z-10 absolute bg-secondary " + className
+				"flex flex-col h-screen w-2/5 max-w-96 min-w-80 shadow-xl z-10 absolute bg-slate-50 " + className
 			}
 		>
-            {children}
+			{children}
 		</aside>
 	);
 }
