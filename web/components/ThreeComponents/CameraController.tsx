7ba3755378f689d4586b30edeefc415197708a3f
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-22 01:55:49 +0100

    aggiunto reference alla camera e cambiato sistema di aggiornamento della posizione del lookat

--- web/components/ThreeComponents/CameraController.tsx
+++ web/components/ThreeComponents/CameraController.tsx
@@ -1,35 +1,53 @@
-import { useKeyboardControls } from "@react-three/drei";
+import { CameraControls, useKeyboardControls } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { Vector3 } from "three";
+import { Ref, RefObject } from "react";
+import { Camera, Vector3 } from "three";
 
 interface CameraControllerProps {
 	setIsNavigating: (boolean: boolean) => void;
 	setCameraPosition: (position: Vector3) => void;
+	cameraRef: RefObject<CameraControls>;
 }
 
 export function CameraController({
 	setIsNavigating,
-	setCameraPosition
+	setCameraPosition,
+	cameraRef,
 }: CameraControllerProps) {
 
 	const [,get] = useKeyboardControls();
-	const camera = useThree();
+	const {camera } = useThree();
+	let prevTarget = new Vector3(); // Store the previous target
 
 	useFrame((state) => {
-		const { forward, backward, left, right, quick } = get()
+		const { forward, backward, left, right, quick } = get();
 		const moveSpeed = quick ? 0.75 : 0.25;
-
-		setIsNavigating(forward || backward || left || right || false);
-
-		state.camera.position.set(
-			camera.camera.position.x + (right ? moveSpeed : 0) + (left ? -moveSpeed : 0),
-			camera.camera.position.y,
-			camera.camera.position.z + (backward ? moveSpeed : 0) + (forward ? -moveSpeed : 0)
+		const target = new Vector3(); // The point to look at
+	
+		// Update position
+		camera.position.set(
+			camera.position.x + (right ? moveSpeed : 0) + (left ? -moveSpeed : 0),
+			camera.position.y,
+			camera.position.z + (backward ? moveSpeed : 0) + (forward ? -moveSpeed : 0)
 		);
-
-		setCameraPosition(state.camera.position);
-		state.camera.updateProjectionMatrix();
+	
+		// Update target based on its previous position and the direction of movement
+		target.set(
+			prevTarget.x + (right ? moveSpeed : 0) + (left ? -moveSpeed : 0),
+			prevTarget.y,
+			prevTarget.z + (backward ? moveSpeed : 0) + (forward ? -moveSpeed : 0)
+		);
+	
+		camera.lookAt(target);
+	
+		// Store the current target for the next frame
+		prevTarget.copy(target);
+	
+		cameraRef.current?.setPosition(camera.position.x, camera.position.y, camera.position.z);
 	});
+	
+	
+	
 	return (
 		<></>
 	);
