85f46a9073a338e1d6c16ec5d8dd9209becb0f1b
Giovanni <2914344+ggardin@users.noreply.github.com>
2024-03-24 18:44:26 +0100

    Corretto overflow

--- web/components/custom/panels/panel.tsx
+++ web/components/custom/panels/panel.tsx
@@ -8,11 +8,10 @@ export default function Panel({children, className} : PanelProps) {
 	return (
 		<aside
 			className = {
-				"flex flex-col h-screen w-2/5 max-w-96 min-w-80 shadow-xl z-10 absolute bg-slate-50 " + className
+				"flex flex-col h-screen w-2/5 max-w-96 min-w-80 overflow-hidden shadow-xl z-10 absolute bg-slate-50 " + className
 			}
 		>
 			{children}
 		</aside>
 	);
 }
-
