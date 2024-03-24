9da679bc8c7ec31f71259920870f4a6bf33cdb22
Giovanni <2914344+ggardin@users.noreply.github.com>
2024-03-24 14:53:19 +0100

    Sostituito apostrofo con entita HTML

--- web/components/custom/panels/Settings/settingsPanel.tsx
+++ web/components/custom/panels/Settings/settingsPanel.tsx
@@ -9,7 +9,7 @@ export default function ZonePanel() {
 			<div className={"m-5"}>
 				<h1 className={"font-bold text-2xl"}>Impostazioni</h1>
 				<h2 className={"font-bold my-4"}>Informazioni</h2>
-				<p className={"text-muted-foreground"}>WMS3 è realizzato da Error_418 (gruppo 7) nell'ambito del corso di Ingegneria del Software (Università degli Studi di Padova, A.A. 2023/2024).</p>
+				<p className={"text-muted-foreground"}>WMS3 è realizzato da Error_418 (gruppo 7) nell&apos;ambito del corso di Ingegneria del Software (Università degli Studi di Padova, A.A. 2023/2024).</p>
 
 				<hr className={"my-6"}/>
 
