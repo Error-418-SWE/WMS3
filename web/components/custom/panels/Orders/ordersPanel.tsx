f867b0e20755d8113b2f54364c29d69ebe97c93f
Giovanni <2914344+ggardin@users.noreply.github.com>
2024-03-25 15:16:51 +0100

    Corretta larghezza pannello ordini

--- web/components/custom/panels/Orders/ordersPanel.tsx
+++ web/components/custom/panels/Orders/ordersPanel.tsx
@@ -6,7 +6,7 @@ export default function OrderPanel() {
 	const { orders } = useOrdersData();
 
 	return (
-		<aside className={"flex flex-col h-screen w-1/5 shadow-xl shrink-0 z-10  absolute bg-secondary"}>
+		<aside className={"flex flex-col h-screen w-2/5 max-w-96 min-w-80 overflow-hidden shadow-xl z-10 absolute bg-slate-50"}>
 			<div className={"flex m-5 items-start"}>
 				<h1 className={"grow font-bold text-2xl"}>Ordini di movimentazione</h1>
 			</div>
