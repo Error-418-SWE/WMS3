658d58eff633cf32167e742acbffecef607a978d
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-28 20:22:15 +0100

    [skip ci] aggiornamento collocamento pattern

---
+++ web/model/FloorStrategy/FloorStrategy.tsx
@@ -0,0 +1,65 @@
+import { readSavedSVG } from "@/ServerActions/SVG/readSavedSVG";
+import { Floor } from "@/model/floor";
+
+export interface FloorStrategy {
+    createFloor(params: URLSearchParams): Promise<Floor>;
+}
+
+export class StandardFloorStrategy implements FloorStrategy {
+    async createFloor(params: URLSearchParams): Promise<Floor> {
+        const length = parseInt(params.get("profondita") || "0");
+        const width = parseInt(params.get("larghezza") || "0");
+        const svg = "";
+        return new Floor(length, width, svg);
+    }
+}
+
+export class CustomFloorStrategy implements FloorStrategy {
+    async createFloor(params: URLSearchParams): Promise<Floor> {
+        const inputSide = parseInt(params.get("latoMaggiore") || "0");
+        const svgString = await readSavedSVG();
+        const parser = new DOMParser();
+        const svgDoc = parser.parseFromString(svgString, 'image/svg+xml');
+        const svgElement = svgDoc.documentElement as unknown as SVGSVGElement;
+        let svgWidth, svgHeight;
+
+        // Controllo se è definita la viewBox
+        if (svgElement.viewBox && svgElement.viewBox.baseVal) {
+            svgWidth = svgElement.viewBox.baseVal.width;
+            svgHeight = svgElement.viewBox.baseVal.height;
+        } else if(svgElement.width && svgElement.height) {
+            // Se non è definita la viewBox, prendo width e height
+            svgWidth = svgElement.width.baseVal.value;
+            svgHeight = svgElement.height.baseVal.value;
+        } else {
+            // Se non è definita neanche width e height, imposto valori di default
+            // per creare un svg di dimensioni [latoMaggiore x latoMaggiore]
+            svgWidth = 1;
+            svgHeight = 1;
+        }
+
+        const aspectRatio = svgWidth / svgHeight;
+        const calculateSide = inputSide / aspectRatio;
+
+        //se l'aspect ratio è maggiore di 1, allora il lato maggiore (inserito dall'utente) è la lunghezza
+        return new Floor( aspectRatio >= 1 ? calculateSide : inputSide, aspectRatio >= 1? inputSide : calculateSide, svgString);
+
+        //input order: length, width
+    }
+}
+
+export class FloorStrategyContext {
+    private strategy: FloorStrategy;
+
+    public constructor(strategy?: FloorStrategy){
+        this.strategy = strategy || new StandardFloorStrategy();
+    }
+
+    public setStrategy(strategy: FloorStrategy){
+        this.strategy = strategy;
+    }
+
+    public async createFloor(params: URLSearchParams): Promise<Floor> {
+        return this.strategy.createFloor(params);
+    }
+}
