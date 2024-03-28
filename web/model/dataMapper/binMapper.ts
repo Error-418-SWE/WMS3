29e76d4fb03a98b42793e2b9a5913a3107bd7d35
Carraro-Riccardo <riccardocarraro69@gmail.com>
2024-03-28 20:21:28 +0100

    [skipci] aggiornamento collocamento pattern

---
+++ web/model/dataMapper/binMapper.ts
@@ -0,0 +1,20 @@
+import { Bin } from "@/model/bin";
+import { ProductMapper } from "@/model/dataMapper/productMapper";
+import { DataMapperInterface } from "./dataMapperInterface";
+
+export class BinMapper implements DataMapperInterface{
+
+	private productMapper: ProductMapper = new ProductMapper();
+
+    public toDomain(json: any): Bin {
+        return new Bin(
+            json.bin_id,
+            json.level_order,
+            json.column_order,
+            json.bin_height,
+            json.bin_length,
+            json.bin_width,
+            json.product ? this.productMapper.toDomain(json.product) : null
+        );
+    }
+}
